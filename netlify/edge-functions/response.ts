2154bd81e66f39e65a4cdb53b55d8c32231d3ccd
Khalid Hossain <khalid@voscra.com>
2022-05-08 21:46:36 -0500

    Now we can delete streamlit repo

--- netlify/edge-functions/response.ts
+++ netlify/edge-functions/response.ts
@@ -1,16 +1,44 @@
 import { Context } from "netlify:edge";
 
 export default async (_request: Request, context: Context) => {
+    const apiToken = Deno.env.get("TOGGL_API_TOKEN");
+    const baseApi = "https://api.track.toggl.com/api/v8/";
+    const headers = {
+      "headers": {
+        "method": "GET",
+        "Authorization": `Basic ${apiToken}`
+      }
+    };
 
-  const apiToken = Deno.env.get("TOGGL_API_TOKEN");
+    const getCurrentTimeEntry = await fetch(`${baseApi}time_entries/current`, headers);
+    const jsonCurrentTimeEntry = await getCurrentTimeEntry.json();
+    const currentTimeEntry = jsonCurrentTimeEntry.data;
 
-  const response = await fetch("https://api.track.toggl.com/api/v8/time_entries/current", {
-    "headers": {
-      "Authorization": `Basic ${apiToken}`
+    // will work for now, but should be refactored
+    if (!currentTimeEntry) {
+      return new Response("Absolutely nothing");
     }
-  });
-  
-  const jsonData = await response.json();
-  return context.json(jsonData.data);
+
+    const getCurrentProject = await fetch(`${baseApi}projects/${currentTimeEntry.pid}`, headers);
+    const jsonCurrentProject = await getCurrentProject.json();
+    const currentProject = jsonCurrentProject.data;
+
+    // this should definitely be frontend code
+    const getCorrectDuration = (duration: number): string => {
+      duration = Date.now() / 1000 + duration;
+      var duration_h = Math.floor(duration / 3600);
+      var duration_m = Math.floor((duration % 3600) / 60);
+      var duration_s = Math.floor((duration % 3600) % 60);
+      return duration_h + "h " + duration_m + "m " + duration_s + "s";
+    }
+    const resp = {
+        "description": currentTimeEntry.description,
+        "project": currentProject.name,
+        "duration": currentTimeEntry.duration,
+    }
+
+    let resp_string = `${resp.description} - ${resp.project} - ${getCorrectDuration(resp.duration)}`;
+
+    return new Response(resp_string);
 
 };
