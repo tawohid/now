fa37fa0810cc558e915fe4143237e2c89ada3077
Khalid Hossain <khalid@voscra.com>
2022-05-10 11:42:59 -0500

    Created front-end skeleton

--- netlify/edge-functions/current.ts
+++ netlify/edge-functions/current.ts
@@ -1,8 +1,10 @@
 import { Context } from "netlify:edge";
 
 export default async (_request: Request, context: Context) => {
+
     const apiToken = Deno.env.get("TOGGL_API_TOKEN");
     const baseApi = "https://api.track.toggl.com/api/v8/";
+
     const headers = {
       "headers": {
         "method": "GET",
@@ -10,35 +12,39 @@ export default async (_request: Request, context: Context) => {
       }
     };
 
+    let resp = {
+      "description": "Nothing",
+      "project": "None",
+      "durationH": 0,
+      "durationM": 0,
+      "durationS": 0,
+    }
+
     const getCurrentTimeEntry = await fetch(`${baseApi}time_entries/current`, headers);
     const jsonCurrentTimeEntry = await getCurrentTimeEntry.json();
     const currentTimeEntry = jsonCurrentTimeEntry.data;
 
-    // will work for now, but should be refactored
     if (!currentTimeEntry) {
-      return new Response("Absolutely nothing");
+      return context.json(resp);
     }
 
     const getCurrentProject = await fetch(`${baseApi}projects/${currentTimeEntry.pid}`, headers);
     const jsonCurrentProject = await getCurrentProject.json();
     const currentProject = jsonCurrentProject.data;
 
-    // this should definitely be frontend code
-    const getCorrectDuration = (duration: number): string => {
-      duration = Date.now() / 1000 + duration;
-      var duration_h = Math.floor(duration / 3600);
-      var duration_m = Math.floor((duration % 3600) / 60);
-      var duration_s = Math.floor((duration % 3600) % 60);
-      return duration_h + "h " + duration_m + "m " + duration_s + "s";
-    }
-    const resp = {
+    const duration = Date.now() / 1000 + currentTimeEntry.duration;
+    const durationH = Math.floor(duration / 3600);
+    const durationM = Math.floor((duration % 3600) / 60);
+    const durationS = Math.floor((duration % 3600) % 60);
+    
+    resp = {
         "description": currentTimeEntry.description,
         "project": currentProject.name,
-        "duration": currentTimeEntry.duration,
+        "durationH": durationH,
+        "durationM": durationM,
+        "durationS": durationS,
     }
 
-    let resp_string = `${resp.description} - ${resp.project} - ${getCorrectDuration(resp.duration)}`;
-
-    return new Response(resp_string);
+    return context.json(resp);
 
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import useFetch from 'react-fetch-hook'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const { isLoading, data, error} = useFetch("/current");
+  const current = data as any;
   return (
-     <h1>Yes, Now.</h1>
-  )
-}
+    <>
+      <h1>What is Khalid doing right now?</h1>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && <h3>{current.description}</h3>}
+      {data && <p>{current.durationH}h - {current.durationM}m - {current.durationS}s</p>}
 
+    </>
+
+  )
+
+};
 export default App
