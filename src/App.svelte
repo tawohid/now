f31bfc25b6bde67a197c051fd7621b4a988fb4b0
Khalid Hossain <khalid@voscra.com>
2022-05-14 23:38:09 -0500

    Changed to Svelte :)

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Current from './lib/Current.svelte'
+</script>
+
+<main>
+  <h1>What is Khalid doing right now?</h1>
+  <Current />
+</main>
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    max-width: 600px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+</style>
---
+++ src/lib/Current.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { dataset_dev } from "svelte/internal"
+
+  // fetch data from the server
+  const getCurrent = (async () => {
+    const response = await fetch('/current')
+    const data = await response.json()
+    return data
+  })()
+
+
+</script>
+
+{#await getCurrent}
+  <p> Loading... </p>
+{:then data}
+  <p>{data.project} / {data.description}</p>
+  <p>{data.durationH}h:{data.durationM}m:{data.durationS}s</p>
+{:catch error}
+  <p>Error: {error}</p>
+{/await}
+
+<style>
+</style>
